4a5fc22a4f31b27f55bab785a894277a43fcfb30
minoruharikae <kamikayamaru5277@gmail.com>
2020-01-16 18:19:20 +0900

    make it better

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,12 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
+    region:          => ENV['S3_REGION'], 
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'areyy'
+  config.fog_directory  =  ENV['S3_BUCKET']
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/areyy'
 end
