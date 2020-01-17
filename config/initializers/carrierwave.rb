4a42effbb8afe2066c876f6e335a1fe6242833af
minoruharikae <kamikayamaru5277@gmail.com>
2020-01-17 14:27:32 +0900

    make it better

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,10 +7,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    secret_key_base: ENV["S3_SECRET_KEY"],
     region: ENV['S3_REGION'], 
     aws_access_key_id: ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET_KEY']
+    aws_secret_access_key: ENV['S3_SECRET_KEY'],
+    secret_key_base: ENV["SECRET_KEY_BASE"]
   }
 
   config.fog_directory  =  ENV['S3_BUCKET']
